75b4e38666f78e96d4e22a4c2808eb90745cffda
Icaro Apolo <apoloraci@gmail.com>
2022-03-22 17:55:14 -0300

    var_dump

--- syntax.php
+++ syntax.php
@@ -1,8 +1,9 @@
 <?php
-// var_dump - Show variable information
+// var_dump -> Show variable information
 $value1 = 3.5;
 $value2 = true;
 $value3 = "basic";
 
 var_dump($value1, $value2, $value3);
+
 ?>
